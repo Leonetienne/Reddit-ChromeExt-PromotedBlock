e772da1d075d1396df4d89db56a358e72b6ce8ff
Leonetienne <leonetienne@hotmail.de>
2021-06-08 12:03:01 +0200

    Added some error checking to make it less likely to remove non-ad elements

--- run.js
+++ run.js
@@ -7,17 +7,41 @@ function Scan()
         shitAd = findings.first();
         for (let i = 0; i < 7; i++)
         {
+            // Check if we are at docroot -> no ad
+            if (shitAd.is(shitAd.parent()))
+                // We are at the docroot. This is not an ad.
+                return;
+            
+            // Does the type match the ad-post template?
+            if (i == 0)
+            {
+                if (shitAd.prop("tagName") != "SPAN")
+                {
+                    // not an ad
+                    return;
+                }
+            }
+            else if (shitAd.prop("tagName") != "DIV")
+            {
+                // not an ad
+                return;
+            }
+
+            // Go one parent-level up
             shitAd = shitAd.parent();
         }
 
-        if (shitAd[0].classList.length == 0)
-        {
-            const shitAdTitle = shitAd.find("h3").html();
-            console.log("Removed shit ad: " + shitAdTitle);
-            shitAd.remove();
-        }
-        else {}
-        // not an ad
+        // The top div of the ad template should not have any classes
+        if (shitAd[0].classList.length != 0)
+            // not an ad
+            return;
+
+        // Fetch the ads title for console output
+        const shitAdTitle = shitAd.find("h3").html();
+        console.log("Removed shit ad: " + shitAdTitle);
+        
+        // Remove that pest
+        shitAd.remove();
     }
 }
 setInterval(Scan, 1);
